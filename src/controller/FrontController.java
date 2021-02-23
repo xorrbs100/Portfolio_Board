3e15146df3451698dd5a94fac548b4463032c319
xorrb <xorrbs100@naver.com>
2021-02-23 21:50:27 +0900

    02-23-주석

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -5,7 +5,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
 
-public class FrontController extends HttpServlet{  //MVC2 모델구조
+public class FrontController extends HttpServlet{  //MVC2 모델구조		컨트롤러를 서블릿 구조로 개발
 
 	private static final long serialVersionUID =1L;
 	String charset = null;
@@ -13,7 +13,7 @@ public class FrontController extends HttpServlet{  //MVC2 모델구조
 	
 	@Override
 	public void init(ServletConfig sc) throws ServletException {	// 서블릿 초기화 , 서브컨트롤러 실행
-		charset = sc.getInitParameter("charset");
+		charset = sc.getInitParameter("charset");			//param의 이름이 charset인 태그의 value값을 추출
 		list = new HashMap<String, Controller>();
 		list.put("/memberInsert.do",new MemberInsertController());
 		list.put("/memberSearch.do",new MemberSearchController());
@@ -31,7 +31,7 @@ public class FrontController extends HttpServlet{  //MVC2 모델구조
 		String contextPath = req.getContextPath();
 		String path= url.substring(contextPath.length());
 				
-		// subController execute메소드 실행
+		// subController execute메소드 실행		- 모든 Controller객체에서는 execute를 재정의하여 기능을 구현함
 		Controller subController = list.get(path);
 		subController.execute(req, res); 
 		
